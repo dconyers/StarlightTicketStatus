43a2afcdd9d39e496ae5c96a9e6b5f9a17cb47a3
Doug Conyers <doug.conyers@gmail.com>
2015-01-28 17:21:19 -0600

    Fixed minor bug after testing. JSON query is working correctly.

--- Available Shows/routes/index.js
+++ Available Shows/routes/index.js
@@ -1,13 +1,31 @@
 ﻿var express = require('express');
 var router = express.Router();
 var spreadsheetAccess = require('../public/javascripts/SpreadsheetAccess');
+var Q = require("q");
+
+var log4js = require("log4js");
+var logger = log4js.getLogger();
 
 
-var status = spreadsheetAccess.getDougState();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express', cool: status,});
+    
+    Q.nfcall(spreadsheetAccess.getJSONShowData).then(function (jsonData) {
+        logger.trace("successfully retrieved JSON data");
+        res.render('index', { title: 'Express', cool: jsonData });
+    }).catch(function (error) {
+        logger.error("Got error trying to render", error);
+    }).done();
+
+
+    //var status = spreadsheetAccess.getJSONShowData(dougRenderer);
+    //function dougRenderer(error, jsonData) {
+//        res.render('index', { title: 'Express', cool: jsonData});
+        
+    //}
+    
 });
+
 
 module.exports = router;
